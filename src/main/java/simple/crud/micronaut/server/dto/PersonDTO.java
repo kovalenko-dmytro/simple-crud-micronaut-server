3b6cdee27d72b23bd4243100b1159616f739b918
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2019-10-01 23:20:23 +0300

    add lombok

--- src/main/java/simple/crud/micronaut/server/dto/PersonDTO.java
+++ src/main/java/simple/crud/micronaut/server/dto/PersonDTO.java
@@ -1,13 +1,14 @@
 package simple.crud.micronaut.server.dto;
 
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.validation.Validated;
+import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Validated
-@Introspected
+@Data
 public class PersonDTO {
 
     @NotBlank
@@ -17,40 +18,9 @@ public class PersonDTO {
     @Size(max = 50)
     private String lastName;
     @NotBlank
+    @Email
     @Size(max = 100)
     private String email;
     @NotBlank
     private String birthday;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
 }
