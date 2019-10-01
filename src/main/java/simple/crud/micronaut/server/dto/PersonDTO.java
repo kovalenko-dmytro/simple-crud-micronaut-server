8548bdb8814efed901bfeb6f2eafd8ed31a5c0f1
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2019-10-01 22:35:43 +0300

    add entity and dto

--- src/main/java/simple/crud/micronaut/server/dto/PersonDTO.java
+++ src/main/java/simple/crud/micronaut/server/dto/PersonDTO.java
@@ -1,5 +1,56 @@
 package simple.crud.micronaut.server.dto;
 
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.validation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Validated
+@Introspected
 public class PersonDTO {
 
+    @NotBlank
+    @Size(max = 30)
+    private String firstName;
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+    @NotBlank
+    @Size(max = 100)
+    private String email;
+    @NotBlank
+    private String birthday;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 }
--- src/main/java/simple/crud/micronaut/server/entity/Person.java
+++ src/main/java/simple/crud/micronaut/server/entity/Person.java
@@ -1,5 +1,70 @@
 package simple.crud.micronaut.server.entity;
 
-public class Person {
+import io.micronaut.core.annotation.Introspected;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Introspected
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long personID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthday;
+
+    public Person(long personID, String firstName, String lastName, String email, LocalDate birthday) {
+        this.personID = personID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthday = birthday;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public long getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(long personID) {
+        this.personID = personID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
 }
